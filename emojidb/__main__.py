f558d7928325f5dbd77c129f2570cc1815858d2f
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2023-03-29 23:28:44 +0200

    Make it more fancy & simple
    - Migrate to PyCord.
    - Add checking if emote id exists.
    - Add checksum check.
    - Remove unnecessary functions.
    - Add some docs.
    - Remove `/meta` route.
    - Remove `/id.extension` route, as ID could have only one entry (one extension).
    - Make more code importable.

--- emojidb/__main__.py
+++ emojidb/__main__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from logging import INFO, basicConfig, getLogger
-from os import listdir, sep
-from os.path import realpath
+from pathlib import Path
 
 from flask import Flask, Response, jsonify, send_file
 from rich.logging import RichHandler
@@ -14,76 +13,55 @@ class EmojiNotFound(NotFound):
 
 app = Flask(__name__)
 
+path = Path.cwd()
+
+
+def all_emoji_files() -> list[Path]:
+    return [d for d in path.iterdir()]
+
+
+def split_ext(file_name: str) -> list[str, str]:
+    """Return a list of file name (which is also its ID) and its extension.
+
+    :param file_name: File full name. For example: "image.webp".
+    :return: List of file name and its extension.
+             For example: ["image", "webp"].
+    """
+    return list(file_name[::1].split('.', maxsplit=1)[::1])
+
+
+def all_emoji_ids() -> list[str]:
+    return [split_ext(file.name)[0] for file in all_emoji_files()]
+
+
+@app.route('/')
+def home() -> Response:
+    # TODO Log app info?
+    return jsonify({'data': 'Hello world!'})
+
+
+@app.route('/emoji')
+def emoji_index() -> Response:
+    return jsonify({'data': [e for e in all_emoji_ids()]})
+
+
+@app.route('/emoji/<emoji_id>')
+def emoji_by_id(emoji_id: str) -> Response:
+    if emoji_id not in all_emoji_ids():
+        raise EmojiNotFound
+    for ext in ['webp', 'png', 'gif', 'jpeg', 'jpg']:
+        if (emoji := path.joinpath(emoji_id, f'.{ext}')).exists():
+            return send_file(emoji)
+    app.aborter(500, description='Emoji exists, but is badly configured.')
+
 
 if __name__ == '__main__':
     basicConfig(
         level=INFO,
         format='%(message)s',
         datefmt='[%x]',
-        handlers=RichHandler(rich_tracebacks=True)
+        handlers=[RichHandler(rich_tracebacks=True)]
     )
     getLogger(app.name).setLevel(INFO)
     getLogger('werkzeug').setLevel(INFO)
-
-    path = f'{realpath(__file__)}assets{sep}img'.replace(
-        '__main__.py', ''
-    )
-
-    def all_emoji_files_names() -> list[str]:
-        return listdir(path)
-
-    def remove_ext(file_name: str) -> list[str, str]:
-        return list(file_name[::1].split('.', maxsplit=1)[::1])
-
-    def remove_ext_from_list(files_list: list[str]) -> list[str]:
-        return list(map(lambda f: remove_ext(f)[0], files_list))
-
-    def emoji_files(emoji_id: str) -> list[str]:
-        return list(filter(
-            lambda e: e.startswith(emoji_id),
-            all_emoji_files_names())
-        )
-    
-    def emoji_exts(emoji_id: str) -> list[str]:
-        return list(map(lambda e: remove_ext(e)[1], emoji_files(emoji_id)))
-
-    def all_emoji_ids():
-        return list(set(remove_ext_from_list(all_emoji_files_names())))
-
-    @app.route('/')
-    def home() -> Response:
-        return jsonify({'data': 'Hello world!'})
-
-    @app.route('/emoji')
-    def emoji_index() -> Response:
-        return jsonify({'data': [e for e in all_emoji_ids()]})
-    
-    @app.route('/emoji/meta/<emoji_id>')
-    def emoji_meta_by_id(emoji_id: str) -> Response:
-        if emoji_id not in all_emoji_ids():
-            raise EmojiNotFound
-        return jsonify({'data': {
-            'id': emoji_id,
-            'extensions': emoji_exts(emoji_id),
-            'sizes': ['112', '112']  # TODO Sizes checking in runtime
-        }})
-    
-    @app.route('/emoji/<emoji_id>')
-    def emoji_by_id(emoji_id: str) -> Response:
-        if emoji_id not in all_emoji_ids():
-            raise EmojiNotFound
-        exts = emoji_exts(emoji_id)
-        for ext in ['webp', 'png', 'gif', 'jpeg', 'jpg']:
-            if ext in exts:
-                return send_file(f'{path}{sep}{emoji_id}.{ext}')
-        app.aborter(500, description='Emoji exists, but is badly configured.')
-    
-    @app.route('/emoji/<emoji_id>.<emoji_ext>')
-    def emoji_qualified(emoji_id: str, emoji_ext: str) -> Response:
-        if emoji_id not in all_emoji_ids():
-            raise EmojiNotFound
-        elif emoji_ext not in emoji_exts(emoji_id):
-            return app.redirect(f'/emoji/{emoji_id}')
-        return send_file(f'{path}{sep}{emoji_id}.{emoji_ext}')
-
     app.run()
--- emojidb/discord_uploader/__main__.py
+++ emojidb/discord_uploader/__main__.py
@@ -1,76 +1,169 @@
-# -*- codin: utf-8 -*-
+# -*- coding: utf-8 -*-
+from hashlib import md5
 from logging import INFO, basicConfig, getLogger
 from os import getenv
 from pathlib import Path
 
-from discord import DiscordException, HTTPException, Intents, Message
-from discord.ext.commands import Bot, Context, EmojiNotFound
+from discord import Intents, Embed, Option, ApplicationContext, Color
+from discord.ext.commands import Bot, Context
 from requests import get as requests_get
 from rich.logging import RichHandler
+
+IMG_FOLDER = Path.cwd().joinpath('assets', 'img')
+
+
+def name_parse(name: str) -> str:
+    return name.replace('-', '_').replace(' ', '_')[:16]
+
+
+def checksum_lookup(content: bytes) -> tuple[bool, Path or None]:
+    """Checks if file exists but under different name.
+
+    :param content: Binary content of a file.
+    :return: If file exists 'True' with file's path,
+    or 'False' and None if it doesn't exist.
+    """
+    new_checksum = md5(content).hexdigest()
+    for dir_ in IMG_FOLDER.iterdir():
+        with open(dir_, 'br') as file:
+            if new_checksum == md5(file.read()).hexdigest():
+                return True, dir_
+    return False, None
+
+
+class EmojiMeta:
+    def __init__(self, emoji_raw: str):
+        emoji_raw = emoji_raw[1:-1].split(':')
+        self.format: str = 'gif' if emoji_raw[0] == 'a' else 'webp'
+        self.name: str = name_parse(emoji_raw[1])
+        self.id: int = int(emoji_raw[2])
+
+    @property
+    def url(self) -> str:
+        return f'https://cdn.discordapp.com/emojis/{self.id}' \
+               f'.{self.format}'
+
+    @property
+    def path(self) -> Path:
+        return IMG_FOLDER.joinpath(f'{self.name}.{self.format}')
+
+    def update_name(self, new_name: str):
+        self.name = name_parse(new_name)
 
 
 def main() -> None:
     rich_log = getLogger('rich')
-    intents = Intents.default()
-    intents.message_content = True
-    bot = Bot(
-        command_prefix=';',
-        help_command=None,
-        intents=intents
-    )
+
+    if not (token := getenv('BOT_TOKEN')):
+        raise RuntimeError('Please set "BOT_TOKEN" environment variable')
+
+    bot = Bot(intents=Intents.default())
 
     @bot.event
     async def on_ready() -> None:
         rich_log.info(f'Logged in as "{bot.user}"')
+        await bot.sync_commands()
+        rich_log.info('Commands synced')
 
-    @bot.hybrid_command(
+    @bot.slash_command(
         name='upload',
-        aliases=['up']
+        description='Yoinks emoji from Discord.'
     )
-    async def upload_emoji(ctx: Context, emoji_raw: str):
-        msg: Message = await ctx.send('Fetching emoji...')
-        is_animated, emoji_name, emoji_id = emoji_raw[1:-1].split(':')
-        emoji_name = emoji_name.replace('-', '_')
-        emoji_id = int(emoji_id)
-        emoji_format = 'gif' if is_animated == 'a' else 'webp'
-        emoji = bot.get_emoji(emoji_id)
-        filename = Path.home().joinpath(
-                'Downloads'
-            ).joinpath(
-                'Emoji DB Uploader'
-            ).joinpath(
-                '{}.{}'.format(emoji_name, emoji_format)
+    async def upload_emoji(
+            ctx: ApplicationContext,
+            emoji_raw: Option(
+                str,
+                'Just emoji',
+                name='emoji',
+                required=True
+            ),
+            emoji_name: Option(
+                str,
+                'Optional name of emoji. Preferred "snake_case" If not'
+                ' present emoji name will be used.',
+                name='emoji_name',
+                max_length=16,
+                default=None,
+                required=False
             )
-        if not emoji:
-            await msg.edit(content='Emoji unavailable, downloading from freinds server...')
-            emoji_url = f' https://cdn.discordapp.com/emojis/{emoji_id}.' + \
-                        f'{emoji_format}?size=128&quality=lossless'
-            await ctx.send(content=f'`{emoji_name}`: {emoji_url}')
-            res = requests_get(emoji_url)
-            if not res.ok:
-                await msg.edit("Firends' server is unavailable")
-                return
-            try:
-                with open(filename, 'x'):
-                    pass
-            except FileExistsError:
+    ):
+        # noinspection GrazieInspection
+        """Emoji uploader.
+
+        :type ctx: Context
+        :type emoji_raw: str
+        :type emoji_name: str or None
+        """
+        await ctx.send_response(embed=Embed(
+            description='Fetching emoji...',
+            color=Color.blurple()
+        ))
+        emoji = EmojiMeta(emoji_raw)
+        if emoji_name is not None:
+            emoji.update_name(emoji_name)
+        filename = IMG_FOLDER.joinpath('{0.name}.{0.format}'.format(emoji))
+        if filename.exists():
+            await ctx.edit(
+                embed=Embed(
+                    description=f'`{emoji.name}` already exists.',
+                    color=Color.blurple()
+                ).add_field(
+                    name='Link',
+                    value='*[Click](https://youtu.be/dQw4w9WgXcQ'
+                          ' "Just click, don\'t think too much")*'
+                ).set_thumbnail(
+                    url=emoji.url
+                )
+            )
+            return
+        await ctx.edit(embed=Embed(
+            description='Downloading from friends server...',
+            color=Color.blurple()
+        ))
+        res = requests_get(emoji.url)
+        if res.status_code != 200:
+            await ctx.edit(embed=Embed(
+                description="Friends' server is unavailable",
+                color=Color.blurple()
+            ))
+            return
+        checksum_exists, file_location = checksum_lookup(res.content)
+        if checksum_exists:
+            await ctx.edit(
+                embed=Embed(
+                    description=f'This emoji (`{emoji.name}`) already exists,'
+                                f' but is named `{file_location.name}`'
+                ).add_field(
+                    name='Link',
+                    value='*[Click](https://youtu.be/dQw4w9WgXcQ'
+                          ' "Just click, don\'t think too much")*'
+                ).set_thumbnail(
+                    url=emoji.url
+                )
+            )
+            return
+        try:
+            with open(filename, 'x'):
                 pass
+        except FileExistsError:
+            pass
+        finally:
             with open(filename, 'bw') as file:
                 file.write(res.content)
-            await msg.edit(content=f'Saved {int(res.headers.get("Content-Length", 0))//1000}kB.')
-        else: 
-            await msg.edit(content='Emoji found, downloading rfom OG servers...')
-            try:
-                bytes_saved: int = await emoji.save(filename)
-            except EmojiNotFound:
-                await msg.edit(content='Just kidding, emoji not found.')
-            except DiscordException or HTTPException as e:
-                await msg.edit(content=f'Saving failed because: ```diff\n- {e.__class__.__name__}:\n- {e}\n```')
-            else:
-                await msg.edit(content=f'Saved {bytes_saved//1000}kB.')
-
-    if not (token := getenv('BOT_TOKEN')):
-        raise RuntimeError('Please set "BOT_TOKEN" environment variable')
+        await ctx.edit(embed=Embed(
+            description='Saved `{}kB` to `{}`.'.format(
+                int(res.headers.get('Content-Length', 0)) // 1000,
+                filename
+            ),
+            color=Color.blurple()
+        ).add_field(
+            name='Link',
+            value='*[Click](https://youtu.be/dQw4w9WgXcQ'
+                  ' "Just click, don\'t think too much")*'
+        ).set_thumbnail(
+            url=emoji.url
+        ))
+        rich_log.info(f'"{ctx.user.name}" downloaded "{filename}"')
 
     bot.run(token)
 
